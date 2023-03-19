527fd8d1af72cae1643ff15cf1baea5e3f80b809
ArtOliRib <99999939+ArtOliRib@users.noreply.github.com>
2023-03-18 22:05:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function trocaCor(cor){
+    
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+
+}
+
+function trocaImg(imagem){
+    
+    let  novaImg = document.querySelector(".iphone");
+    novaImg.src =  imagem;
+
+}
